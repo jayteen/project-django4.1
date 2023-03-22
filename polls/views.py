2bf0feea5296aa63bf1f131cd95a82f0769fd665
jayteen <62333772+jayteen@users.noreply.github.com>
2023-03-22 23:51:52 +0800

    feat:3.表单和通用视图

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
 
 
 # Create your views here.
@@ -31,9 +34,61 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    # response = "You're looking at the results of question %s."
+    # return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+# 改良视图
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.POST 是一个类字典对象，让你可以通过关键字的名字获取提交的数据。
+        # 这个例子中 request.POST['choice'] 以字符串形式返回选择的 Choice 的 ID。
+        # request.POST 的值永远是字符串。
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        # 如果在 request.POST['choice'] 数据中没有提供 choice，POST 将引发一个 KeyError。
+        # 上面的代码检查 KeyError，如果没有给出 choice 将重新显示 Question 表单和一个错误信息。
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # 正如上面的Python注释指出的，在成功处理POST数据后，你应该总是返回一个
+        # HttpResponseRedirect。这不是Django的特殊要求，这是那些优秀网站在开发实践中形成的共识。
+        # 在这个例子中，我们在HttpResponseRedirect的构造函数中使用reverse()函数。
+        # 这个函数避免了我们在视图函数中硬编码URL。
+        # 它需要我们给出我们想要跳转的视图的名字和该视图所对应的URL
+        # 模式中需要给该视图提供的参数。
+        # 在本例中，使用在教程第3部分中设定的URLconf，
+        # reverse()调用将返回一个这样的字符 '/polls/3/results/'
+        # 其中 3 是 question.id 的值。重定向的 URL 将调用 'results' 视图来显示最终的页面。
